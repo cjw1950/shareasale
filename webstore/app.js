59f6f314c3961a881ce17aa3cd5b32e1316221d5
Charles Williams <williams_c1950@yahoo.com>
2018-07-12 07:43:02 -0400

    additional routes

--- webstore/app.js
+++ webstore/app.js
@@ -13,6 +13,35 @@ var wines  = require('./routes/wines');
 var restaurants = require('./routes/restaurants');
 var sports = require('./routes/sports');
 var glasses = require('./routes/glasses')
+// food
+var fakeCigarette = require('./routes/fakeCigarette');
+var fakeCigarette2 = require('./routes/fakeCigarette2');
+var  caviar = require('./routes/caviar');
+var  cubanfood = require('./routes/cubanfoodmarket'); 
+var  juices = require('./routes/exoticjuices'); 
+var  spices = require('./routes/foodspicecomplete');
+var  gourmet = require('./routes/gourmat_food'); 
+var  glutenfree = require('./routes/katzglutenfreebakedgoods');
+var  survivalfood = require('./routes/survivalfood');
+var  tea= require('./routes/teaanequipment'); 
+var  weightloss = require('./routes/weightlossplan');
+var  juicetanks = require('./routes/juicetanks');
+//
+var  asiantech  = require('./routes/asiancameraaccessories'); 
+var  batterAccesories1 = require('./routes/batteryaccessories');                                              
+var  batterAccesories2= require('./routes/batteryaccessories2');                                             
+var  cable = require('./routes/cablesandaccerories'); 
+var  cellphone = require('./routes/cellphones');  
+var  chinesetech = require('./routes/chineseshopqs_electronic');  
+var  computerAccesories = require('./routes/computeraccessories'); 
+var  harddrives1 = require('./routes/harddrivescomputer');
+var  harddrives2= require('./routes/hardrives');  
+var  smartphone= require('./routes/smartphones');    
+var  headphones= require('./routes/wirelessheadphones');
+var  speakers = require('./routes/wirelessspeakers');
+var  spy= require('./routes/spyingequipment'); 
+
+
 
 var app = express();
 
@@ -23,7 +52,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,6 +66,34 @@ app.use('/glasses',glasses);
 app.use('/sports',sports);
 app.use('/restaurants',restaurants);
 app.use('/wines',wines);
+app.use('/fakeCigarette', fakeCigarette);
+app.use('/fakeCigarette2', fakeCigarette2);
+//grouping food
+// app.use('/caviar',caviar );
+// app.use('/cubanfood',cubanfood);
+// app.use('/juices',juices );
+// app.use('/spices', spices);
+// app.use('/gourmet ',gourmet);
+// app.use('/glutenfree', glutenfree);
+// app.use('/survivalfood ',survivalfood);
+// app.use('/tea',tea );
+// app.use('/weightloss',weightloss);
+// app.use('/juicetanks',juicetanks);
+// //tech
+
+// app.use('/asiantech',asiantech );
+// app.use('/battery1',batterAccesories1 );
+// app.use('/battery2',batterAccesories1 );
+// app.use('/cable',cable );
+// app.use('/cellphone',cellphone );
+// app.use('/computerAccesories',computerAccesories  );
+// app.use('/chinesetech',chinesetech );
+// app.use('/harddrives1',harddrives1 );
+// app.use('/harddrives2',harddrives2 );
+// app.use('/smartphone',smartphone);
+// app.use('/headphones', headphones );
+// app.use('/speakers',speakers );
+// app.use('/spy',spy );
 
 
 
---
+++ webstore/routes/televisionsets.js
@@ -0,0 +1,29 @@
+'use strict';
+var express = require('express');
+var cors = require('cors');
+var pool = require('../helper/ConnectionPool');
+var router = express.Router();
+var options={
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-methods": "GET",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+  router.get('/',cors(options),function(req,res,next){
+      
+     
+      pool.getConnection(function(err,connection){
+    if(err) throw err; 
+        connection.query("select * from televisionsets ",function(error,results,fields){
+            if (error) throw error;
+            var data=JSON.stringify(results);
+            var s =JSON.parse(data)
+            res.json(s);
+            connection.release();
+        }); 
+
+    });
+    
+    
+  });
+  module.exports = router;
